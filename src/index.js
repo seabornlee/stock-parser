8636914c162eea9001ee9dda0131622ba8cad371
Seaborn Lee <seabornlee.cn@gmail.com>
2020-03-28 15:35:30 +0800

    chore: remove unused dependencies and change port

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const StockParser = require('./parser')
 
-let server = app.listen(5000, function () {
+let server = app.listen(80, function () {
   let host = server.address().address;
   let port = server.address().port;
   console.log('Your App is running at http://%s:%s', host, port);
