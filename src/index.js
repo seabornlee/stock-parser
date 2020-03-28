3fd997523d4be54d66a12b664f3679ef281da66f
Seaborn Lee <seabornlee.cn@gmail.com>
2020-03-28 14:37:06 +0800

    It works!

--- src/index.js
+++ src/index.js
@@ -9,8 +9,13 @@ let server = app.listen(5000, function () {
 });
 
 app.get('/', async (req, res) => {
-  const data = await new StockParser().fetchStock()
+  const data = await new StockParser().parse()
   const string = data.join('\r\n')
+  res.contentType('text/plain');
+  res.set({
+    'Content-Type': 'text/plain',
+    'Content-Disposition': 'attachment; filename=stocks.txt' 
+  })
   res.send(string);
 });
 
--- src/parser.js
+++ src/parser.js
@@ -1,37 +1,34 @@
-const Nightmare = require("nightmare");
-const nightmare = Nightmare();
-const cheerio = require("cheerio");
-
-const url =
-  "http://calc.tdx.com.cn:7616/site/kggx/fx_bkrdyc.html?color=%23%23tdxbk%23%23&bkcolor=%23%23tdxbkcolor%23%23";
+const fetch = require('node-fetch')
 
 module.exports = class StockParser {
-  parse() {}
-
-  async fetchStock() {
-    let data = [];
-    await nightmare
-      .goto(url)
-      .wait(".grid-table")
-      .evaluate(() => {
-        return document.querySelector(".grid-table").innerHTML;
-      })
-      .then(html => {
-        data.push(this.parseStock(html));
-      })
-      .catch(error => {
-        console.log(error);
-      });
-    return data;
+  async parse() {
+    const catagories = await this.fetchCatagories()
+    const stocks = []
+    for (let c of catagories) {
+      const list = await this.fetchStocks(c[0])
+      list.forEach(s => stocks.push(s[1]))
+    }
+    return stocks
   }
 
-  parseStock(data) {
-    const $ = cheerio.load(data);
-    const texts = $.text().split(" ");
-    const numbers = new RegExp(/\d+\.\d+/);
-    const names = texts.filter(
-      t => t !== "" && !t.includes("%") && !numbers.test(t)
-    );
-    return names;
+  async fetchStocks(code) {
+    let result
+    await fetch("http://calc.tdx.com.cn:7616/TQLEX?Entry=HQServ.hq_nlp", {"credentials":"include","headers":{"accept":"text/plain, */*; q=0.01","accept-language":"zh-CN,zh;q=0.9,en-US;q=0.8,en;q=0.7","cache-control":"no-cache","content-type":"application/x-www-form-urlencoded; charset=UTF-8","pragma":"no-cache","x-requested-with":"XMLHttpRequest"},"referrer":"http://calc.tdx.com.cn:7616/site/kggx/fx_bkrdyc.html?color=%23%23tdxbk%23%23&bkcolor=%23%23tdxbkcolor%23%23","referrerPolicy":"no-referrer-when-downgrade","body":"[{\"ReqId\":\"2200\",\"BkCode\":\"" + code + "\",\"Sort\":\"1\",\"SortType\":\"0\",\"PageSize\":\"50\",\"modname\":\"module_misc.dll\"}]","method":"POST","mode":"cors"})
+      .then(res => res.json())
+      .then(data => {
+        result = data.ResultSets[1].Content
+      })
+
+    return result
+  }
+
+  async fetchCatagories() {
+    let result
+    await fetch("http://calc.tdx.com.cn:7616/TQLEX?Entry=HQServ.hq_nlp", {"credentials":"include","headers":{"accept":"text/plain, */*; q=0.01","accept-language":"zh-CN,zh;q=0.9,en-US;q=0.8,en;q=0.7","cache-control":"no-cache","content-type":"application/x-www-form-urlencoded; charset=UTF-8","pragma":"no-cache","x-requested-with":"XMLHttpRequest"},"referrer":"http://calc.tdx.com.cn:7616/site/kggx/fx_bkrdyc.html?color=%23%23tdxbk%23%23&bkcolor=%23%23tdxbkcolor%23%23","referrerPolicy":"no-referrer-when-downgrade","body":"[{\"ReqId\":\"2100\",\"UpCount\":\"5\",\"DownCount\":\"5\",\"modname\":\"module_misc.dll\"}]","method":"POST","mode":"cors"})
+      .then(res => res.json())
+      .then(data => {
+        result = data.ResultSets[1].Content 
+      })
+    return result
   }
 };
