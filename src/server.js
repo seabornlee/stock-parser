d897ff24915fe2f7ea764363c62a297fcea8ec6d
Seaborn Lee <seabornlee.cn@gmail.com>
2021-01-05 22:20:28 +0800

    feat: remove old parser and API

--- src/server.js
+++ src/server.js
@@ -1,48 +1,26 @@
 const express = require("express");
 const app = express();
-const Service = require("./service");
 const KplService = require("./kpl_service");
-const fs = require("fs")
-const iconv = require('iconv-lite');
+const fs = require("fs");
+const iconv = require("iconv-lite");
 
-const service = new Service()
-const kplService = new KplService()
-
-app.get("/:type", async (req, res) => {
-  const type = req.params.type
-  if (type != 'code' && type != 'name') {
-    res.status(404).send()
-    return
-  }
-
-  let data
-  if (type === 'code')
-    data = await service.getCodes();
-  else
-    data = await service.getNames();
-
-  const string = data.join("\r\n");
-  res.contentType("text/plain");
-  res.set({
-    "Content-Disposition": `attachment; filename=stocks-${req.params.type}.BLK`
-  });
-  res.send(string);
-});
-
-app.get("/code/xml", async (req, res) => {
-  res.attachment('看多板块.xml')
-  res.send(renderXML(await service.getCodes()));
-});
+const kplService = new KplService();
 
 function renderXML(data) {
-  const buffer = fs.readFileSync('./template.xml')
-  const xml = iconv.encode(iconv.decode(buffer, 'gb2312'), 'utf8').toString()
-  const nodes = data.map(code => `<stk setcode="${code.charAt(0)}" code="${code.substring(1)}"/>`)
+  const buffer = fs.readFileSync("./template.xml");
+  const xml = iconv.encode(iconv.decode(buffer, "gb2312"), "utf8").toString();
+  const nodes = data.map(
+    (code) => `<stk setcode="${code.charAt(0)}" code="${code.substring(1)}"/>`
+  );
   return xml.replace("${codes}", nodes.join(""));
 }
 
 app.get("/kpl/code/xml/:blockIndex/:topCount", async (req, res) => {
-  res.attachment('开盘啦板块.xml')
-  res.send(renderXML(await kplService.getCodes(req.params.blockIndex, req.params.topCount)));
+  res.attachment("开盘啦板块.xml");
+  res.send(
+    renderXML(
+      await kplService.getCodes(req.params.blockIndex, req.params.topCount)
+    )
+  );
 });
-module.exports = app
+module.exports = app;
--- test/api.test.js
+++ test/api.test.js
@@ -1,53 +1,22 @@
 const request = require("supertest");
 const app = require("../src/server.js");
-const Service = require("../src/service");
 
 describe("Api", () => {
-  describe("/code", () => {
-    it("responds with 200", done => {
-      request(app)
-        .get("/code")
-        .expect(200, done);
-    });
-  });
-
-  describe("/name", () => {
-    it("responds with 200", done => {
-      request(app)
-        .get("/name")
-        .expect(200, done);
-    });
-  });
-
-  describe("/code/xml", () => {
-    it("responds with 200", () => {
-      return request(app)
-          .get("/code/xml")
-          .expect(200)
-          .expect("Content-Type", /xml/)
-          .then(res => {
-            expect(res.text).not.toBe('')
-          })
-    });
-  });
-
   describe("/kpl/code/xml", () => {
     it("responds with 200", () => {
       return request(app)
         .get("/kpl/code/xml/1/2")
         .expect(200)
         .expect("Content-Type", /xml/)
-        .then(res => {
-          expect(res.text.includes("stk setcode=")).toBeTruthy()
-        })
+        .then((res) => {
+          expect(res.text.includes("stk setcode=")).toBeTruthy();
+        });
     });
   });
 
   describe("/others", () => {
-    it("responds 404", done => {
-      request(app)
-        .get("/others")
-        .expect(404, done);
+    it("responds 404", (done) => {
+      request(app).get("/others").expect(404, done);
     });
   });
 });
